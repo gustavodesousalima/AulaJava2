9e387da1acadc45920178597c3c4e5e7791da14d
Gustavo de Sousa Lima <gustavdesousalima@gmail.com>
2024-11-11 22:20:30 -0300

    First Commit

---
+++ src/main/java/comandoscondicionais/Numeropar.java
@@ -0,0 +1,17 @@
+package comandoscondicionais;
+import java.util.Scanner;
+public class Numeropar {
+
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("entre com um numero");
+        float numero = entrada.nextFloat();
+
+        if(numero % 2 == 0){
+            System.out.println("O numero é par:" + numero);
+        }else{
+            System.out.println("O numero é impar:" + numero);
+        }
+    }
+}
---
+++ src/main/java/comandoscondicionais/votos.java
@@ -0,0 +1,22 @@
+package comandoscondicionais;
+import java.util.Scanner;
+
+
+public class votos {
+    public static void main(String[] args){
+
+        Scanner entradavoto = new Scanner(System.in);
+
+        System.out.println("Digite sua idade");
+
+        int idade = entradavoto.nextInt();
+
+        if (idade >= 18 && idade < 65){
+            System.out.println("Voto obrigatório");
+        }else if(idade >= 16 || idade >= 65 ){
+            System.out.println("Voto opcional");
+        }else{
+            System.out.println("Não pode votar");
+        }
+    }
+}
